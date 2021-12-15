5992aa727b02f8cbbe979285753e82c0959a1971
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2021-12-15 12:31:43 +0100

    Update q4.r

--- Question 4/Code/q4.r
+++ Question 4/Code/q4.r
@@ -1,11 +1,14 @@
+# Read from the file
 data <- read.delim("Question4_3.txt")
 head(data)
+# Hist plot
 hist(data$Times, 
      breaks = seq(15,40,5), 
      xlab = "Time(Minute)", 
      main = "Frequency"
 )
-input <- data[,c('Times', Static_Analysis')]
+# Boxplot
+input <- data[,c('Times', 'Static_Analysis')]
 boxplot(Times~Static_Analysis, 
      data, 
      ylab="Time", 
@@ -13,3 +16,5 @@ boxplot(Times~Static_Analysis,
      names=c("Static Analysis","No Static Analysis"), 
      main = "Time with and without Static Analysis"
 )
+# Split the table base on Static_Analysis for t-test and cohens'd effect size calculation
+df = split(data, data$Static_Analysis)
