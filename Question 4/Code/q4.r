9caf590d84b1529364d99033262b54f8635c9ca2
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2021-12-15 11:16:35 +0100

    Update q4.r

--- Question 4/Code/q4.r
+++ Question 4/Code/q4.r
@@ -1,5 +1,14 @@
 data <- read.delim("Question4_3.txt")
 head(data)
-hist(data$Times, breaks = seq(15,40,5), xlab = "Time(Minute)", main = "Frequency")
+hist(data$Times, 
+     breaks = seq(15,40,5), 
+     xlab = "Time(Minute)", 
+     main = "Frequency"
+)
 input <- data[,c('Times', Static_Analysis')]
-boxplot(Times~Static_Analysis, data, ylab="Time", xlab = "Static Analysis status", names=c("Static Analysis","No Static Analysis"), main = "Time with and without Static Analysis")
+boxplot(Times~Static_Analysis, 
+     data, ylab="Time", 
+     xlab = "Static Analysis status", 
+     names=c("Static Analysis","No Static Analysis"), 
+     main = "Time with and without Static Analysis"
+)
