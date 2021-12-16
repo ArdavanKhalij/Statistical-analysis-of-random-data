8e137c921a823ff3eb757bedb95debea4ca4fcf7
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2021-12-16 11:17:49 +0100

    Update q3.r

--- Question 3/Code/q3.r
+++ Question 3/Code/q3.r
@@ -1,2 +1,6 @@
-dataq3 <- read.delim("~/Desktop/StatisticsMWO/Question3_3.txt")
+# Libraries
+library(ggplot2)
+library(dplyr)
+# Read from input file
+dataq3 <- read.delim("Question3_3.txt")
 head(dataq3)
