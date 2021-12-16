baf9e35f88f10fd58934edb93c42c73a0d98ef5a
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2021-12-16 11:50:45 +0100

    Update q3.r

--- Question 3/Code/q3.r
+++ Question 3/Code/q3.r
@@ -5,3 +5,9 @@ library(gapminder)
 # Read from input file
 dataq3 <- read.delim("Question3_3.txt")
 head(dataq3)
+# Split
+df2 = split(dataq3, dataq3$Interfaces, dataq3$Responses)
+# Split again
+df21 = split(df2[[1]], df2[[1]]$Responses)
+df22 = split(df2[[2]], df2[[2]]$Responses)
+df23 = split(df2[[3]], df2[[3]]$Responses)
