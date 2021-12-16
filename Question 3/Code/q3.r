9feec19d38ee9db938a04b779ab161a47a1e5b23
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2021-12-16 15:22:10 +0100

    Update q3.r

--- Question 3/Code/q3.r
+++ Question 3/Code/q3.r
@@ -11,3 +11,12 @@ df2 = split(dataq3, dataq3$Interfaces, dataq3$Responses)
 df21 = split(df2[[1]], df2[[1]]$Responses)
 df22 = split(df2[[2]], df2[[2]]$Responses)
 df23 = split(df2[[3]], df2[[3]]$Responses)
+# Bubble plot
+ggplot(dataq3, 
+       aes(x=dataq3$Interfaces, 
+           y=dataq3$Responses
+          )
+      ) 
++ geom_point(size = 25, alpha = 0.06) 
++ xlab("Interfaces") + ylab("Responses") 
++ ggtitle("Response per Interface")
