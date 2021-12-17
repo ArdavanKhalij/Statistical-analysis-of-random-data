eb1251029753ade90fcce54f9d6d63b80b3584ee
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2021-12-17 09:29:31 +0100

    Create q2.r

---
+++ Question 2/Code/q2.r
@@ -0,0 +1,7 @@
+# Import data
+dataq2 <- read.delim("Question2_1.txt")
+# Convert dataset
+dataq2$CPU_Failure <- paste(dataq2$CPU, dataq2$Failure, sep="_")
+data_new <- dataq2[c("Temperature", "CPU_Failure")]
+# Box plot
+boxplot(Temperature~CPU_Failure, data_new, ylab="Time", xlab = "Static Analysis status", names=c("AMD_FALSE","AMD_True", "Intel_FALSE", "Intel_TRUE"), main = "Time with and without Static Analysis", notch=TRUE)
